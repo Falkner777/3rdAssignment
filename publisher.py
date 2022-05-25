1a9fc65ed6af78ade7deb761fed675df0d51b732
Dimitris Verginis <d.verginis777@gmail.com>
2022-05-25 12:58:38 +0300

    Update publisher.py

--- publisher.py
+++ publisher.py
@@ -7,7 +7,6 @@ from paho.mqtt import client as mqtt_client
 broker = 'broker.emqx.io'
 port = 1883
 topic = "python/mqtt"
-# generate client ID with pub prefix randomly
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 username = 'emqx'
 password = 'public'
